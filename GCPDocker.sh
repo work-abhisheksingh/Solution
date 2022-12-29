768559fde8441d3d20bb843ba6b4e198cb6f5b52
Abhishek Singh <115094053+work-abhisheksingh@users.noreply.github.com>
2022-12-30 03:57:43 +0530

    Update and rename Cloud Server Commands to GCPDocker.sh

---
+++ GCPDocker.sh
@@ -0,0 +1,21 @@
+sudo apt install npm
+sudo apt install git
+npm install express
+npm install node
+git clone link
+ls
+cd foldername
+ls
+node server.js
+##Google Cloud Docker Commands
+sudo apt update
+docker -v
+sudo apt install docker.io
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo systemctl status docker
+sudo docker images
+sudo docker pull hello-world
+sudo docker pull nginx
+sudo docker run --name abcserver -d -p 8080:80 nginx
+sudo docker kill 
